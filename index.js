62f5f7252ad60e13ca330b347f3f03912625aa61
David Kim <mail.ddk@gmail.com>
2017-11-13 23:05:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function destructivelyRemoveFirstDriver() {
 }
 
 function appendDriver(Broom) {
-  return drivers.splice(0, 3, 'Broom');
+  return drivers.splice(2, 0, 'Broom');
 }
 
 function prependDriver(Arnold) {
